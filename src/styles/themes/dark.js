245f0f409b03effeb2d96eccfdb0d28bc69189a6
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-03-17 22:20:34 -0300

    feat: added light and dark theme definitions
    Added definitions as objects for the light & dark theming

---
+++ src/styles/themes/dark.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#7159c1',
+    secondary: '#39375B',
+    background: '#333',
+    text: 'rgba(255, 255, 255, 0.87)',
+  },
+};
---
+++ src/styles/themes/light.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#7159c1',
+    secondary: '#39375B',
+    background: '#f5f5f5',
+    text: '#333',
+  },
+};
