494ea90eb3e2c9e2bdcfc03f3761948f93b1b6f7
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-03-17 23:25:02 -0300

    chore: eslint rules & slider library

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     'react/jsx-one-expression-per-line': 'off',
     'global-require': 'off',
     'react/jsx-props-no-spreading': 'off',
+    'no-shadow': 'off',
   },
   settings: {
     'import/resolver': {
