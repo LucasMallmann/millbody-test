652974d9b3809ee3b2de10945db9e22efefd3df2
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-03-17 22:23:50 -0300

    feat: themeProvider & ThemeContext to all children components
    added the theme provider so that the children components may have access to the app theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components';
+
+import light from '~/styles/themes/light';
+// import dark from '~/styles/themes/dark';
 
 import Routes from '~/routes';
 
 const App = () => {
+  // const [theme, setTheme] = useState(light);
+
   return (
     <NavigationContainer>
-      <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <ThemeProvider theme={light}>
+        <Routes />
+      </ThemeProvider>
     </NavigationContainer>
   );
 };
