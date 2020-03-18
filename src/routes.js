3f069334481a99cdc22a4320b7899e74293a1c3b
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-03-17 22:24:35 -0300

    refactor: routes definitions
    added a second page in the route definition

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TouchableOpacity, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ThemeContext } from 'styled-components';
 
 import Home from '~/pages/Home';
 import Second from '~/pages/Second';
@@ -7,9 +9,30 @@ import Second from '~/pages/Second';
 const Stack = createStackNavigator();
 
 const Routes = () => {
+  const { colors } = useContext(ThemeContext);
+
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.primary,
+        },
+        headerTintColor: '#f8f8f8',
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerTitleAlign: 'center',
+          headerRight: () => (
+            <TouchableOpacity style={{ marginRight: 10 }}>
+              <Text>toggle</Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
       <Stack.Screen name="Second" component={Second} />
     </Stack.Navigator>
   );
